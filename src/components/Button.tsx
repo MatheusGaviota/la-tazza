9b488eddb4a15da3c14cb93d0a0c9b19482743a4
MarcosAlves90 <MarcosAlves25@proton.me>
2025-10-10 16:38:02 -0300

    components/Button: add ghost variants and update styling

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 type ButtonProps = {
   text: string;
   href: string;
-  variant?: "accent" | "fore";
+  variant?: "accent" | "fore" | "ghost-accent" | "ghost-fore" | "ghost";
   className?: string;
 };
 
@@ -12,7 +12,16 @@ export default function Button({ text, href, variant = "accent", className = "" 
 
   const baseClasses =
     "inline-block px-6 py-1 rounded-md transition-all hover:opacity-90 active:scale-95";
-  const variantClasses = variant === "accent" ? "bg-accent text-white" : "bg-foreground text-[var(--background)]";
+  const variantClasses =
+    variant === "accent"
+      ? "bg-accent text-background border-2 border-accent"
+      : variant === "fore"
+      ? "bg-foreground text-background border-2 border-foreground"
+      : variant === "ghost-accent"
+      ? "bg-transparent text-background border-2 border-accent"
+      : variant === "ghost-fore"
+      ? "bg-transparent text-background border-2 border-foreground"
+      : ""
   const combinedClasses = `${baseClasses} ${variantClasses} ${className}`.trim();
 
   if (isExternal) {
