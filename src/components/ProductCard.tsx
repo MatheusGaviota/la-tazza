67da2edda41227ab0fb7c00bd8581ce3bcefa459
Marcos Alves <marcos.a.lopes7701@gmail.com>
2025-10-20 15:44:49 -0300

    feat(ui): add ProductCard component

---
+++ src/components/ProductCard.tsx
@@ -0,0 +1,44 @@
+import Image from 'next/image';
+import Button from './Button';
+
+interface ProductCardProps {
+    imageUrl: string;
+    title: string;
+    description: string;
+    onAddToCart?: () => void;
+}
+
+export default function ProductCard({
+    imageUrl,
+    title,
+    description,
+    onAddToCart
+}: ProductCardProps) {
+    return (
+        <article className="border-3 rounded-xl border-foreground overflow-hidden h-full flex flex-col">
+            <div className="relative w-full h-48 sm:h-56 md:h-64">
+                <Image
+                    src={imageUrl}
+                    alt={title}
+                    fill
+                    sizes="(max-width: 640px) 100vw, (max-width: 1024px) 50vw, (max-width: 1280px) 33vw, 25vw"
+                    className="rounded-t-xl object-cover"
+                />
+            </div>
+            <div className='p-4 sm:p-5 md:p-6 space-y-2 sm:space-y-3 flex-1 flex flex-col'>
+                <h3 className="font-alumni font-semibold text-3xl sm:text-4xl text-foreground text-center">
+                {title}
+                </h3>
+                <p className="text-center text-sm sm:text-base text-foreground flex-1">
+                    {description}
+                </p>
+                <Button
+                    text="Adicionar ao Carrinho"
+                    variant="accent"
+                    className="w-full text-center mt-auto"
+                    onClick={onAddToCart}
+            />
+            </div>
+        </article>
+    );
+}
